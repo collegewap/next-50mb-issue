b5bd6dc1fdcf2f178ad30e523c83a50af4cc04c6
Abhishek EH <collegewap@gmail.com>
2022-01-26 20:09:22 +0530

    remove next/image reference

--- components/blog/BlogCover.tsx
+++ components/blog/BlogCover.tsx
@@ -1,16 +1,9 @@
-import Image from 'next/image'
-import React, { useContext } from 'react'
-import BlogContext from '../../context/BlogContext'
+import React from 'react'
 const BlogCover = (): JSX.Element => {
-  const blogContext = useContext(BlogContext)
-  const {
-    coverImage: { src, alt, blurDataURL },
-  } = blogContext
-
   return (
     <div className="max-w-xl lg:max-w-2xl m-auto flex pt-0 lg:pt-6  lg:px-6 px-0 lg:py-12 py-6 lg:flex-row flex-col items-center">
       <div className="lg:h-[335px] sm:h-80 h-64 relative z-[2]  w-full lg:rounded-md overflow-hidden">
-        <Image
+        {/* <Image
           src={src}
           alt={alt}
           priority
@@ -27,7 +20,7 @@ const BlogCover = (): JSX.Element => {
           layout="fill"
           objectFit="cover"
           placeholder={`${blurDataURL ? 'blur' : 'empty'}`}
-        />
+        /> */}
       </div>
     </div>
   )
